f14a9faeffce57db16f353f79433abb124f09f3b
123liuziming <448918299@qq.com>
2021-04-09 21:53:19 +0800

    fix bug

--- src/components/custom-properties-panel.vue
+++ src/components/custom-properties-panel.vue
@@ -1,247 +1,251 @@
 <template>
-    <div class="containers" ref="content">
-        <div class="canvas" ref="canvas"></div>
-        <properties-view v-if="bpmnModeler" :modeler="bpmnModeler"></properties-view>
-        <el-button @click="uploadDrl">上传</el-button>
-    </div>
+  <div class="containers" ref="content">
+    <div class="canvas" ref="canvas"></div>
+    <properties-view v-if="bpmnModeler" :modeler="bpmnModeler"></properties-view>
+    <el-button @click="uploadDrl">上传</el-button>
+  </div>
 </template>
 
 <script>
-    // 引入相关的依赖
-    // import BpmnViewer from 'bpmn-js'
-    import BpmnModeler from 'bpmn-js/lib/Modeler'
-    import PropertiesView from './custom-properties-panel/PropertiesView'
-    import customModule from './custom/index'
-    import {options} from "../assets/js/condition";
+// 引入相关的依赖
+// import BpmnViewer from 'bpmn-js'
+import BpmnModeler from 'bpmn-js/lib/Modeler'
+import PropertiesView from './custom-properties-panel/PropertiesView'
+import customModule from './custom/index'
+import {options} from "../assets/js/condition";
 
-    export default {
-        name: '',
-        components: {
-            PropertiesView
-        },
-        // 生命周期 - 创建完成（可以访问当前this实例）
-        created() {
-        },
-        // 生命周期 - 载入后, Vue 实例挂载到实际的 DOM 操作完成，一般在该过程进行 Ajax 交互
-        mounted() {
-            this.init();
-            this.clear();
-        },
-        data() {
-            return {
-                // bpmn建模器
-                bpmnModeler: null,
-                container: null,
-                canvas: null,
-                xml: "",
-            }
-        },
-        // 方法集合
-        methods: {
-            uploadDrl() {
-                console.log("xml");
-                console.log(this.xml);
-                let parser = new DOMParser();
-                let xmlDoc = parser.parseFromString(this.xml, "text/xml");
-                let sequences = xmlDoc.getElementsByTagName("sequenceFlow");
-                let gateWayAttribute = new Map();
-                for (let i = 0; i < sequences.length; ++i) {
-                    let key = sequences[i].getAttribute("sourceRef") + "," + sequences[i].getAttribute("targetRef");
-                    gateWayAttribute.set(key, sequences[i]);
-                }
-                // 限定开始节点只有一个
-                let startNode = xmlDoc.getElementsByTagName("startEvent")[0];
-                let tasks = xmlDoc.getElementsByTagName("task");
-                let taskAttribute = new Map();
-                for (let i = 0; i < tasks.length; ++i) {
-                    taskAttribute.set(tasks[i].getAttribute("id"), tasks[i]);
-                }
-                let edges = xmlDoc.getElementsByTagName("sequenceFlow");
-                console.log("edges", edges);
-                let linkedMap = new Map();
-                for (let i = 0; i < edges.length; ++i) {
-                    let edge = edges[i];
-                    let from = edge.getAttribute("sourceRef");
-                    let to = edge.getAttribute("targetRef");
-                    if (!linkedMap.has(from)) {
-                        linkedMap.set(from, []);
-                    }
-                    let val = linkedMap.get(from);
-                    val.push(to);
-                    linkedMap.set(from, val);
-                }
-                console.log("linked map ", linkedMap);
-                let result = [];
-                this.dfs(linkedMap, result, [], startNode.getAttribute("id"));
-                let script = this.generateDrlScript("test", result, gateWayAttribute, taskAttribute);
-                let data = new FormData();
-                data.append('droolStr',script);
-                data.append('id',"test");
-                data.append("xml", this.xml);
-                this.$axios.post(`drools/drl/upload`,data)
-                    .then(res=>{
-                        alert("success")
-                    })
-            },
-
-            generateDrlScript(ruleName, graph, gateWayAttribute, taskAttribute) {
-                let drlStr = "package droolRule\nimport com.lzm.health.constant.Sex;\n" +
-                    "import com.lzm.health.model.User;\n";
-                console.log(graph);
-                let result = new String(drlStr);
-                for (let i = 0; i < graph.length; ++i) {
-                    result += this.generateConditions(graph[i], gateWayAttribute, taskAttribute, ruleName + i);
-                }
-                return result;
-            },
-            replaceXml(str) {
-                str = str.replace(/,/g, "&&")
-                    .replace(/&#38;&#38;/g, "&&")
-                    .replace(/&#62;/g, ">")
-                    .replace(/&#60;/g, "<")
-                    .replace(/User/g, "$user");
-                return str;
-            },
-            generateConditions(edges, gateWayAttribute, taskAttribute, ruleName) {
-                console.log(gateWayAttribute, taskAttribute);
-                let result = "rule" + "\"" + ruleName + "\"\nwhen\n$user:User(";
-                console.log("edges " + edges)
-                for (let i = 1; i < edges.length - 1; ++i) {
-                    let key = edges[i] + "," + edges[i + 1];
-                    let val = gateWayAttribute.get(key);
-                    console.log("val is " + val);
-                    let ageCondition = val.getAttribute("conditionAge");
-                    if (ageCondition) {
-                        ageCondition = this.replaceXml(ageCondition)
-                    }
-                    let sexCondition = val.getAttribute("conditionSex");
-                    if (sexCondition) {
-                        sexCondition = this.replaceXml(sexCondition);
-                    }
-                    let nutritionCondition = val.getAttribute("conditionNutrition");
-                    if (nutritionCondition) {
-                        for (let option of options) {
-                            nutritionCondition = nutritionCondition.replace(option.label + ",", "");
-                        }
-                        nutritionCondition = nutritionCondition.replace(",", " , ");
-                    }
-                    if (nutritionCondition) {
-                        nutritionCondition = this.replaceXml(nutritionCondition);
-                    }
-                    if (ageCondition) {
-                        result += ageCondition;
-                    }
-                    if (ageCondition && ageCondition.length && sexCondition && sexCondition.length) {
-                        result += (" && $user.sex==" + sexCondition);
-                    }
-                    if (sexCondition && sexCondition.length && nutritionCondition && nutritionCondition.length) {
-                        result += (" && " + nutritionCondition);
-                    }
-                }
-                result += ")\nthen\n$user.setFoods(\"" + taskAttribute.get(edges[edges.length - 1]).getAttribute("foodIdxs") + "\");\nend\n";
-                //result = result("User", "$user");
-                return result;
-            },
-            dfs(linkedMap, result, tmp, current) {
-                if (!linkedMap.has(current)) {
-                    tmp.push(current);
-                    result.push([...tmp]);
-                    tmp.pop();
-                    return;
-                }
-                let tos = linkedMap.get(current);
-                for (let i = 0; i < tos.length; ++i) {
-                    let to = tos[i];
-                    tmp.push(current);
-                    this.dfs(linkedMap, result, tmp, to);
-                    tmp.pop();
-                }
-            },
-            clear() {
-                let classes = [
-                    'bpmn-icon-lasso-tool',
-                    'bpmn-icon-space-tool',
-                    'bpmn-icon-intermediate-event-none',
-                    'bpmn-icon-subprocess-expanded',
-                    'bpmn-icon-data-object',
-                    'bpmn-icon-participant',
-                    'bpmn-icon-group',
-                    'bpmn-icon-data-store',
-                    'bpmn-icon-intermediate-event-none',
-                ];
-                for (let i = 0; i < classes.length; ++i) {
-                    let x = document.getElementsByClassName(classes[i]);
-                    for (let j = 0; j < x.length; ++j) {
-                        x[j].style.display = 'none';
-                    }
-                }
-            },
-            init() {
-                // 获取到属性ref为“content”的dom节点
-                this.container = this.$refs.content
-                // 获取到属性ref为“canvas”的dom节点
-                const canvas = this.$refs.canvas
-                // 建模
-                this.bpmnModeler = new BpmnModeler({
-                    container: canvas
-                })
-                this.createNewDiagram()
-            },
-            createNewDiagram() {
-                // 将字符串转换成图显示出来
-                this.bpmnModeler.importXML(this.xmlStr.xmlStr, err => {
-                    if (err) {
-                        // console.error(err)
-                    } else {
-                        // 这里是成功之后的回调, 可以在这里做一系列事情
-                        this.xml = this.xmlStr.xmlStr;
-                        this.success();
-                    }
-                })
-            },
-            success() {
-                // 给图绑定事件，当图有发生改变就会触发这个事件
-                const that = this
-                this.bpmnModeler.on('commandStack.changed', function () {
-                    that.saveDiagram(function (err, xml) {
-                        console.log(xml);
-                        that.xml = xml;
-                        let edits = document.getElementsByClassName('djs-direct-editing-content')
-                        for (let i = 0; i < edits.length; ++i) {
-                            edits[i].style.display = "none"
-                        }
-                    })
-                })
-            },
-            // 下载为bpmn格式,done是个函数，调用的时候传入的
-            saveDiagram(done) {
-                // 把传入的done再传给bpmn原型的saveXML函数调用
-                this.bpmnModeler.saveXML({format: true}, function (err, xml) {
-                    done(err, xml)
-                })
-            }
-        },
-        // 计算属性
-        computed: {}
+export default {
+  name: '',
+  components: {
+    PropertiesView
+  },
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+  },
+  // 生命周期 - 载入后, Vue 实例挂载到实际的 DOM 操作完成，一般在该过程进行 Ajax 交互
+  mounted() {
+    this.init();
+    this.clear();
+  },
+  data() {
+    return {
+      // bpmn建模器
+      bpmnModeler: null,
+      container: null,
+      canvas: null,
+      xml: "",
     }
+  },
+  // 方法集合
+  methods: {
+    uploadDrl() {
+      console.log("xml");
+      console.log(this.xml);
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(this.xml, "text/xml");
+      let sequences = xmlDoc.getElementsByTagName("sequenceFlow");
+      let gateWayAttribute = new Map();
+      for (let i = 0; i < sequences.length; ++i) {
+        let key = sequences[i].getAttribute("sourceRef") + "," + sequences[i].getAttribute("targetRef");
+        gateWayAttribute.set(key, sequences[i]);
+      }
+      // 限定开始节点只有一个
+      let startNode = xmlDoc.getElementsByTagName("startEvent")[0];
+      let tasks = xmlDoc.getElementsByTagName("task");
+      let taskAttribute = new Map();
+      for (let i = 0; i < tasks.length; ++i) {
+        taskAttribute.set(tasks[i].getAttribute("id"), tasks[i]);
+      }
+      let edges = xmlDoc.getElementsByTagName("sequenceFlow");
+      console.log("edges", edges);
+      let linkedMap = new Map();
+      for (let i = 0; i < edges.length; ++i) {
+        let edge = edges[i];
+        let from = edge.getAttribute("sourceRef");
+        let to = edge.getAttribute("targetRef");
+        if (!linkedMap.has(from)) {
+          linkedMap.set(from, []);
+        }
+        let val = linkedMap.get(from);
+        val.push(to);
+        linkedMap.set(from, val);
+      }
+      console.log("linked map ", linkedMap);
+      let result = [];
+      this.dfs(linkedMap, result, [], startNode.getAttribute("id"));
+      let script = this.generateDrlScript("test", result, gateWayAttribute, taskAttribute);
+      let data = new FormData();
+      data.append('droolStr', script);
+      data.append('id', "test");
+      data.append("xml", this.xml);
+      this.$axios.post(`drools/drl/upload`, data)
+          .then(res => {
+            alert("success")
+          })
+    },
+
+    generateDrlScript(ruleName, graph, gateWayAttribute, taskAttribute) {
+      let drlStr = "package droolRule\nimport com.lzm.health.constant.Sex;\n" +
+          "import com.lzm.health.model.User;\n";
+      console.log(graph);
+      let result = new String(drlStr);
+      for (let i = 0; i < graph.length; ++i) {
+        result += this.generateConditions(graph[i], gateWayAttribute, taskAttribute, ruleName + i);
+      }
+      return result;
+    },
+    replaceXml(str) {
+      str = str.replace(/,/g, "&&")
+          .replace(/&#38;&#38;/g, "&&")
+          .replace(/&#62;/g, ">")
+          .replace(/&#60;/g, "<")
+          .replace(/User/g, "$user");
+      return str;
+    },
+    generateConditions(edges, gateWayAttribute, taskAttribute, ruleName) {
+      console.log(gateWayAttribute, taskAttribute);
+      let result = "rule" + "\"" + ruleName + "\"\nwhen\n$user:User(";
+      console.log("edges " + edges)
+      for (let i = 1; i < edges.length - 1; ++i) {
+        let key = edges[i] + "," + edges[i + 1];
+        let val = gateWayAttribute.get(key);
+        let ageCondition = val.getAttribute("conditionAge");
+        if (ageCondition) {
+          ageCondition = this.replaceXml(ageCondition)
+        }
+        let sexCondition = val.getAttribute("conditionSex");
+        if (sexCondition) {
+          sexCondition = this.replaceXml(sexCondition);
+        }
+        let nutritionCondition = val.getAttribute("conditionNutrition");
+        if (nutritionCondition) {
+          for (let option of options) {
+            nutritionCondition = nutritionCondition.replace(option.label + ",", "");
+          }
+          nutritionCondition = nutritionCondition.replace(",", " , ");
+        }
+        if (nutritionCondition) {
+          nutritionCondition = this.replaceXml(nutritionCondition);
+        }
+        if (ageCondition) {
+          result += (ageCondition + " && ");
+        }
+        if (sexCondition && sexCondition.length) {
+          result += ("$user.sex==" + sexCondition + " && ");
+        }
+        if (nutritionCondition && nutritionCondition.length) {
+          result += (nutritionCondition + " && ");
+        }
+        console.log("condition is below ");
+        console.log(ageCondition, sexCondition, nutritionCondition);
+      }
+      if (result.endsWith(" && ")) {
+        result = result.slice(0, -4);
+      }
+      result += ")\nthen\n$user.setFoods(\"" + taskAttribute.get(edges[edges.length - 1]).getAttribute("foodIdxs") + "\");\nend\n";
+      //result = result("User", "$user");
+      return result;
+    },
+    dfs(linkedMap, result, tmp, current) {
+      if (!linkedMap.has(current)) {
+        tmp.push(current);
+        result.push([...tmp]);
+        tmp.pop();
+        return;
+      }
+      let tos = linkedMap.get(current);
+      for (let i = 0; i < tos.length; ++i) {
+        let to = tos[i];
+        tmp.push(current);
+        this.dfs(linkedMap, result, tmp, to);
+        tmp.pop();
+      }
+    },
+    clear() {
+      let classes = [
+        'bpmn-icon-lasso-tool',
+        'bpmn-icon-space-tool',
+        'bpmn-icon-intermediate-event-none',
+        'bpmn-icon-subprocess-expanded',
+        'bpmn-icon-data-object',
+        'bpmn-icon-participant',
+        'bpmn-icon-group',
+        'bpmn-icon-data-store',
+        'bpmn-icon-intermediate-event-none',
+      ];
+      for (let i = 0; i < classes.length; ++i) {
+        let x = document.getElementsByClassName(classes[i]);
+        for (let j = 0; j < x.length; ++j) {
+          x[j].style.display = 'none';
+        }
+      }
+    },
+    init() {
+      // 获取到属性ref为“content”的dom节点
+      this.container = this.$refs.content
+      // 获取到属性ref为“canvas”的dom节点
+      const canvas = this.$refs.canvas
+      // 建模
+      this.bpmnModeler = new BpmnModeler({
+        container: canvas
+      })
+      this.createNewDiagram()
+    },
+    createNewDiagram() {
+      // 将字符串转换成图显示出来
+      this.bpmnModeler.importXML(this.xmlStr.xmlStr, err => {
+        if (err) {
+          // console.error(err)
+        } else {
+          // 这里是成功之后的回调, 可以在这里做一系列事情
+          this.xml = this.xmlStr.xmlStr;
+          this.success();
+        }
+      })
+    },
+    success() {
+      // 给图绑定事件，当图有发生改变就会触发这个事件
+      const that = this
+      this.bpmnModeler.on('commandStack.changed', function () {
+        that.saveDiagram(function (err, xml) {
+          console.log(xml);
+          that.xml = xml;
+          let edits = document.getElementsByClassName('djs-direct-editing-content')
+          for (let i = 0; i < edits.length; ++i) {
+            edits[i].style.display = "none"
+          }
+        })
+      })
+    },
+    // 下载为bpmn格式,done是个函数，调用的时候传入的
+    saveDiagram(done) {
+      // 把传入的done再传给bpmn原型的saveXML函数调用
+      this.bpmnModeler.saveXML({format: true}, function (err, xml) {
+        done(err, xml)
+      })
+    }
+  },
+  // 计算属性
+  computed: {}
+}
 </script>
 
 <style scoped>
-    .containers {
-        background-color: #ffffff;
-        width: 100%;
-        height: calc(100vh - 52px);
-    }
+.containers {
+  background-color: #ffffff;
+  width: 100%;
+  height: calc(100vh - 52px);
+}
 
-    .canvas {
-        width: 100%;
-        height: 100%;
-    }
+.canvas {
+  width: 100%;
+  height: 100%;
+}
 
-    .panel {
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 300px;
-    }
+.panel {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 300px;
+}
 </style>
--- src/mock/xmlStr.js
+++ src/mock/xmlStr.js
@@ -4,12 +4,42 @@ export default {xmlStr : `
 <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" id="sid-38422fae-e03e-43a3-bef4-bd33b32041b2" targetNamespace="http://bpmn.io/bpmn" exporter="bpmn-js (https://demo.bpmn.io)" exporterVersion="5.1.2">
   <process id="Process_1" isExecutable="false">
     <startEvent id="StartEvent_1y45yut" name="开始">
-      <outgoing>SequenceFlow_0h21x7r</outgoing>
+      <outgoing>SequenceFlow_1k8r5d8</outgoing>
     </startEvent>
-    <task id="Task_1hcentk" foods="" foodIdxs="1,2">
-      <incoming>SequenceFlow_0h21x7r</incoming>
+    <exclusiveGateway id="ExclusiveGateway_1efrt3x">
+      <incoming>SequenceFlow_1k8r5d8</incoming>
+      <outgoing>SequenceFlow_09zmw44</outgoing>
+      <outgoing>SequenceFlow_02y9kct</outgoing>
+    </exclusiveGateway>
+    <exclusiveGateway id="ExclusiveGateway_0zqj6zo">
+      <incoming>SequenceFlow_09zmw44</incoming>
+      <outgoing>SequenceFlow_1gvnxw2</outgoing>
+      <outgoing>SequenceFlow_0jvxdm7</outgoing>
+    </exclusiveGateway>
+    <exclusiveGateway id="ExclusiveGateway_0y2ztfz">
+      <incoming>SequenceFlow_02y9kct</incoming>
+      <outgoing>SequenceFlow_0nqx5bg</outgoing>
+      <outgoing>SequenceFlow_0we4f62</outgoing>
+    </exclusiveGateway>
+    <sequenceFlow id="SequenceFlow_1k8r5d8" sourceRef="StartEvent_1y45yut" targetRef="ExclusiveGateway_1efrt3x" />
+    <sequenceFlow id="SequenceFlow_09zmw44" name="性别判定 判定元素:" sourceRef="ExclusiveGateway_1efrt3x" targetRef="ExclusiveGateway_0zqj6zo" conditionSex="Sex.MALE" />
+    <sequenceFlow id="SequenceFlow_02y9kct" name="性别判定 判定元素:" sourceRef="ExclusiveGateway_1efrt3x" targetRef="ExclusiveGateway_0y2ztfz" conditionSex="Sex.FEMALE" />
+    <task id="Task_17gbcq4" foods="[object Object],[object Object]" foodIdxs="4,5">
+      <incoming>SequenceFlow_1gvnxw2</incoming>
     </task>
-    <sequenceFlow id="SequenceFlow_0h21x7r" sourceRef="StartEvent_1y45yut" targetRef="Task_1hcentk" />
+    <task id="Task_1t56fdr" foods="[object Object]" foodIdxs="72">
+      <incoming>SequenceFlow_0jvxdm7</incoming>
+    </task>
+    <task id="Task_1p68mh2" foods="[object Object]" foodIdxs="99">
+      <incoming>SequenceFlow_0nqx5bg</incoming>
+    </task>
+    <task id="Task_0pn09o9" foods="[object Object]" foodIdxs="100">
+      <incoming>SequenceFlow_0we4f62</incoming>
+    </task>
+    <sequenceFlow id="SequenceFlow_1gvnxw2" name=" 年龄判定 判定元素:" sourceRef="ExclusiveGateway_0zqj6zo" targetRef="Task_17gbcq4" conditionNutrition="水,User.water &#62; 50 &#38;&#38; User.water &#60; 80" conditionAge="User.age &#38;#62; 18" />
+    <sequenceFlow id="SequenceFlow_0jvxdm7" name=" 年龄判定 判定元素:" sourceRef="ExclusiveGateway_0zqj6zo" targetRef="Task_1t56fdr" conditionNutrition="水,User.water &#60; 10" conditionAge="User.age &#38;#62; 1 &#38;#38;&#38;#38; User.age &#38;#60; 2" />
+    <sequenceFlow id="SequenceFlow_0nqx5bg" name=" 年龄判定 判定元素:" sourceRef="ExclusiveGateway_0y2ztfz" targetRef="Task_1p68mh2" conditionNutrition="水,User.water &#60; 10" conditionAge="User.age &#38;#62; 2 &#38;#38;&#38;#38; User.age &#38;#60; 3" />
+    <sequenceFlow id="SequenceFlow_0we4f62" name=" 年龄判定 判定元素:" sourceRef="ExclusiveGateway_0y2ztfz" targetRef="Task_0pn09o9" conditionNutrition="水,User.water &#60; 10" conditionAge="User.age &#38;#62; 3 &#38;#38;&#38;#38; User.age &#38;#60; 4" />
   </process>
   <bpmndi:BPMNDiagram id="BpmnDiagram_1">
     <bpmndi:BPMNPlane id="BpmnPlane_1" bpmnElement="Process_1">
@@ -19,12 +49,79 @@ export default {xmlStr : `
           <omgdc:Bounds x="160" y="145" width="22" height="14" />
         </bpmndi:BPMNLabel>
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id="Task_1hcentk_di" bpmnElement="Task_1hcentk">
-        <omgdc:Bounds x="240" y="80" width="100" height="80" />
+      <bpmndi:BPMNShape id="ExclusiveGateway_1efrt3x_di" bpmnElement="ExclusiveGateway_1efrt3x" isMarkerVisible="true">
+        <omgdc:Bounds x="255" y="105" width="50" height="50" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNEdge id="SequenceFlow_0h21x7r_di" bpmnElement="SequenceFlow_0h21x7r">
+      <bpmndi:BPMNShape id="ExclusiveGateway_0zqj6zo_di" bpmnElement="ExclusiveGateway_0zqj6zo" isMarkerVisible="true">
+        <omgdc:Bounds x="375" y="45" width="50" height="50" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="ExclusiveGateway_0y2ztfz_di" bpmnElement="ExclusiveGateway_0y2ztfz" isMarkerVisible="true">
+        <omgdc:Bounds x="375" y="205" width="50" height="50" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNEdge id="SequenceFlow_1k8r5d8_di" bpmnElement="SequenceFlow_1k8r5d8">
         <omgdi:waypoint x="188" y="120" />
-        <omgdi:waypoint x="240" y="120" />
+        <omgdi:waypoint x="222" y="120" />
+        <omgdi:waypoint x="222" y="130" />
+        <omgdi:waypoint x="255" y="130" />
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="SequenceFlow_09zmw44_di" bpmnElement="SequenceFlow_09zmw44">
+        <omgdi:waypoint x="280" y="105" />
+        <omgdi:waypoint x="280" y="70" />
+        <omgdi:waypoint x="375" y="70" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="271" y="85" width="48" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="SequenceFlow_02y9kct_di" bpmnElement="SequenceFlow_02y9kct">
+        <omgdi:waypoint x="280" y="155" />
+        <omgdi:waypoint x="280" y="230" />
+        <omgdi:waypoint x="375" y="230" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="271" y="190" width="48" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNShape id="Task_17gbcq4_di" bpmnElement="Task_17gbcq4">
+        <omgdc:Bounds x="500" y="30" width="100" height="80" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Task_1t56fdr_di" bpmnElement="Task_1t56fdr">
+        <omgdc:Bounds x="500" y="140" width="100" height="80" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Task_1p68mh2_di" bpmnElement="Task_1p68mh2">
+        <omgdc:Bounds x="500" y="270" width="100" height="80" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Task_0pn09o9_di" bpmnElement="Task_0pn09o9">
+        <omgdc:Bounds x="500" y="410" width="100" height="80" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNEdge id="SequenceFlow_1gvnxw2_di" bpmnElement="SequenceFlow_1gvnxw2">
+        <omgdi:waypoint x="425" y="70" />
+        <omgdi:waypoint x="500" y="70" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="423" y="52" width="81" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="SequenceFlow_0jvxdm7_di" bpmnElement="SequenceFlow_0jvxdm7">
+        <omgdi:waypoint x="400" y="95" />
+        <omgdi:waypoint x="400" y="180" />
+        <omgdi:waypoint x="500" y="180" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="375" y="135" width="81" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="SequenceFlow_0nqx5bg_di" bpmnElement="SequenceFlow_0nqx5bg">
+        <omgdi:waypoint x="400" y="255" />
+        <omgdi:waypoint x="400" y="310" />
+        <omgdi:waypoint x="500" y="310" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="375" y="280" width="81" height="27" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNEdge>
+      <bpmndi:BPMNEdge id="SequenceFlow_0we4f62_di" bpmnElement="SequenceFlow_0we4f62">
+        <omgdi:waypoint x="400" y="255" />
+        <omgdi:waypoint x="400" y="470" />
+        <omgdi:waypoint x="500" y="470" />
+        <bpmndi:BPMNLabel>
+          <omgdc:Bounds x="375" y="360" width="81" height="27" />
+        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
     </bpmndi:BPMNPlane>
   </bpmndi:BPMNDiagram>
